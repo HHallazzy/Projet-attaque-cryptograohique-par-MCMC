6b3d1da73772003cef959e3d62c49194ea746d5c
Ferreira Baudin Ethan <ethan.ferreira--baudin@etud.u-picardie.fr>
2026-09-02 11:48:49 +0200

    modifications des commentaires de substitution.py

--- src/substitution.py
+++ src/substitution.py
@@ -24,13 +24,13 @@ def verifier_cle(cle: str) -> bool:
     Contrôle que la clé fait bien 26 caractères et contient 
     exactement une fois chaque lettre de l'alphabet.
     """
-    # Vérification 1 : La longueur
+    # On vérifie la longueur
     if len(cle) != 26:
         print("Erreur : La clé doit contenir exactement 26 caractères.")
         return False
         
-    # Vérification 2 : Les caractères uniques correspondent exactement à A-Z
-    # L'utilisation de set() élimine les doublons automatiquement.
+    # on vérifie que les caractères uniques correspondent exactement à A-Z
+    # on utilise set() pour éliminer les doublons automatiquement.
     if set(cle) != set(ALPHABET_26):
         print("Erreur : La clé contient des doublons ou des caractères non autorisés.")
         return False
@@ -42,12 +42,9 @@ def chiffrer_texte(texte_clair: str, cle: str) -> str:
     Tiret 2 : Appliquer la clé à un texte clair pour obtenir un cryptogramme.
     Remplace chaque lettre selon la permutation de la clé, en ignorant les espaces.
     """
-    # Création d'une table de correspondance (mapping) entre l'alphabet normal et la clé
+    # On crée une table de correspondance entre l'alphabet normal et la clé
     table_substitution = str.maketrans(ALPHABET_26, cle)
     
-    # La méthode translate parcourt la chaîne en C (très rapide) et remplace
-    # les caractères selon la table. Tout caractère absent de la table (l'espace)
-    # reste strictement inchangé.
     texte_chiffre = texte_clair.translate(table_substitution)
     
     return texte_chiffre
@@ -67,10 +64,10 @@ def dechiffrer_texte(cryptogramme: str, cle: str) -> str:
     """
     Tiret 4 : Déchiffrer un texte chiffré avec une clé connue.
     """
-    # 1. On calcule la clé inverse
+    # On calcule la clé inverse
     cle_inverse = inverser_cle(cle)
     
-    # 2. On applique exactement la même mécanique optimisée que pour le chiffrement
+    # On applique exactement la même mécanique optimisée que pour le chiffrement
     table_substitution = str.maketrans(ALPHABET_26, cle_inverse)
     texte_dechiffre = cryptogramme.translate(table_substitution)
     
